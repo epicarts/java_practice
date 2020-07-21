d07837d57e52bed1b0922640bb4003be4402f425
YoungHo Choi <0505zxc@gmail.com>
2020-07-21 23:09:46 +0900

    자바 modifier 와 생성자

---
+++ StaticTest.java
@@ -0,0 +1,6 @@
+public class StaticTest {
+    public static void main(String[] args) {
+        System.out.println("눈이 " + Dice.dotCount + "개인 주사위를 굴린다.");
+        System.out.println(Dice.playGame());
+    }
+}
