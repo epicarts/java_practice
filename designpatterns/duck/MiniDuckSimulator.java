cc75a0eb1de0a614047165a608ce9f0b3deba65b
YoungHo Choi <0505zxc@gmail.com>
2020-10-01 20:30:51 +0900

    모형오리 클래스 구현

--- designpatterns/duck/MiniDuckSimulator.java
+++ designpatterns/duck/MiniDuckSimulator.java
@@ -1,10 +1,19 @@
 package designpatterns.duck;
 
-//작은 오리
-public class MiniDuckSimulator extends  MallardDuck {
+//오리 시뮬레이터
+public class MiniDuckSimulator extends MallardDuck {
     public static void main(String[] args) {
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        System.out.println("\n");
+        Duck model = new ModelDuck();
+        model.performQuack();
+        model.performFly(); // 생성자를 이용해 초기화.
+
+        //세터를 사용해 이후에도 변경.
+        model.setFlyBehavior(new FlyRocketPowered()); //로켓 객체를 생성하여 참조값을 전달.
+        model.performFly();
     }
 }
--- designpatterns/duck/ModelDuck.java
+++ designpatterns/duck/ModelDuck.java
@@ -1,4 +1,14 @@
 package designpatterns.duck;
 
-public class Modelduck {
+//모형 오리 클래스
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay(); // 날지못하고
+        quackBehavior = new MuteQuack(); // 짖지 못합니다.
+    }
+
+    @Override
+    public void display() {
+        System.out.println("모형 오리입니다.");
+    }
 }
